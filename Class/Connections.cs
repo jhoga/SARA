02447dac684d9b49bad8dc68e9dc7cfe0bd72811
Jerry Hogan <jhoga@salisburync.gov>
2018-05-16 16:45:16 -0400

    Update Connections.cs

--- Class/Connections.cs
+++ Class/Connections.cs
@@ -8,9 +8,9 @@ namespace SaraD2.Class
 {
     public class Connections
     {
-        public static String csSQL = "Data Source=cbsql1;Database=hddata;UID=HD;pwd=helpdesk";
-        public static String csFAC = "Data Source=cbsql1;Database=facilities;UID=inet;pwd=kj4hf";
-        public static String csSig = "Data Source=cbsql1;Database=StreetSigns;UID=HD;pwd=helpdesk";
-        public static String csSign = "Data Source=cbsql1;Initial Catalog=streetsigns;UID=HD;pwd=helpdesk";
+        public static String csSQL = "Data Source=cbsql1;Database=hddata;UID=HD;pwd=";
+        public static String csFAC = "Data Source=cbsql1;Database=facilities;UID=inet;pwd=";
+        public static String csSig = "Data Source=cbsql1;Database=StreetSigns;UID=HD;pwd=";
+        public static String csSign = "Data Source=cbsql1;Initial Catalog=streetsigns;UID=HD;pwd=";
     }
 }
